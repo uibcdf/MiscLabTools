e777b873350abb9bf2b6215caee68a79c1154da0
Diego Prada <prada.gracia@gmail.com>
2018-06-05 13:07:15 -0500

    First repo structure to be pushed to Anaconda -building files not ready yet-

---
+++ setup.py
@@ -0,0 +1,16 @@
+import distutils.extension
+from setuptools import setup
+
+setup(
+    name='pytools_uibcdf',
+    version='0.1',
+    author='UIBCDF Lab',
+    author_email='uibcdf@gmail.com',
+    packages=['pytools_uibcdf'],
+    scripts=[],
+    url='http://uibcdf.org',
+    download_url ='https://github.com/uibcdf/PyTools_uibcdf',
+    license='MIT',
+    description="---",
+    long_description="---",
+)
